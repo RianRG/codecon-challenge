67ab3297a8b3023841368cc4972513e8567ffdf2
RianRG <rianrgomes@gmail.com>
2025-06-29 13:25:39 -0300

    feat: team-insights route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -35,12 +35,11 @@ export async function Routes(app){
           country: each.country,
           total: 1
         })
+      } else{
+        acm["countries"].forEach(k =>{
+          if(k.country==each.country) k.total+=1;
+        })
       }
-      
-
-      acm["countries"].forEach(k =>{
-        if(k.country==each.country) k.total+=1;
-      })
       return acm;
     }, {countries: []})
 
@@ -50,5 +49,43 @@ export async function Routes(app){
     const end = Date.now();
 
     return res.status(200).send({ countries: topCountries.countries, timeProcessing: `${Math.floor((end - start))} ms` })
+  })
+
+  app.get('/team-insights', async (req, res) =>{
+
+    const start = Date.now();
+    let seenTeams = {}
+    let teams = users.reduce((acm, each) =>{
+      if(!seenTeams[each.team.name]){
+        seenTeams[each.team.name]=1;
+        acm.push({
+          team: each.team.name,
+          total_members: 1,
+          leaders: each.team.leader ? 1 : 0,
+          completed_projects: each.team.projects.filter(project => project.completed).length,
+          active_percentage: each.active 
+        })
+      } else{
+
+        acm.forEach(k =>{
+          if(k.team === each.team.name){
+            k.total_members+=1;
+            k.leaders += each.team.leader ? 1 : 0;
+            k.completed_projects+= each.team.projects.filter(project => project.completed).length;
+            k.active_percentage += each.active ? 1 : 0
+          }
+        })
+      }
+      return acm;
+    }, [])
+
+    teams.map(k =>{
+      k.active_percentage = +(k.active_percentage / k.total_members * 100).toFixed(2);
+    })
+
+    const end = Date.now();
+
+    return res.status(200).send({ teams, timeProcessing: `${Math.floor((end - start))} ms` })
+    
   })
 }
