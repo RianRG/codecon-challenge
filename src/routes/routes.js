73f1a8ce3f452e2a5343f338e331f66e9e8187dd
RianRG <rianrgomes@gmail.com>
2025-06-29 11:34:03 -0300

    feat: second route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,21 @@ import path from 'path'
 let users = [];
 export async function Routes(app){
   app.post('/users', async (req, res) =>{
+    const start = Date.now();
     let data = await readFileSync(path.join(__dirname, 'assets/usuarios.json'));
     data = data.toString();
     users = JSON.parse(data);
-    return res.status(201).send({ msg: 'created Sucessfully!' })
+    const end = Date.now();
+    return res.status(201).send({ msg: 'created Sucessfully!', timeProcessing: `${Math.floor((end - start))} ms`})
+  })
+
+
+  app.get('/superusers', async (req, res) =>{
+    const start = Date.now();
+    const filteredUsers = users.filter(user =>{
+      return user.score>=900 && user.active
+    })
+    const end = Date.now();
+    return res.status(200).send({ superUsers: filteredUsers, timeProcessing: `${Math.floor((end - start))} ms` })
   })
 }
